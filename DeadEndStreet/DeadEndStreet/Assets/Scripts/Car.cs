08fb1bc5a489fc7bb6b085ad62f3f89c662861e1
Caio Calisto <caiocesarcalisto@gmail.com>
2022-10-11 14:02:16 +0100

    Added user Input front / back

--- DeadEndStreet/DeadEndStreet/Assets/Scripts/Car.cs
+++ DeadEndStreet/DeadEndStreet/Assets/Scripts/Car.cs
@@ -1,21 +1,19 @@
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class Car : MonoBehaviour
 {
-    public Car()
-    {
-        Physics.gravity = new Vector3(0, -1.0F, 0);
-    }
-    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Physics.gravity = new Vector3(0, -1.0F, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * horizontalInput * Time.deltaTime * 5);   
     }
 }
